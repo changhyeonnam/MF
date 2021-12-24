0bac1f5c92b5693e03a6335daaa467024970a0b5
changhyeonnam <hj04143@gmail.com>
2021-12-24 10:45:50 +0900

    update utils.py

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from utils import MovieLens
 from torch.utils.data import DataLoader
 from model.MF import MatrixFactorization
 from train import Train
+from evaluation import RMSELoss
+
 device = torch.device('cuda' if torch.cuda.is_available()  else 'cpu')
 print('device:',device)
 
 parser = argparse.ArgumentParser(description="Matrix Factorization with movieLens")
 parser.add_argument('-e','--epochs',default=1,type=int)
 parser.add_argument('-b','--batch',default=32,type=int)
-parser.add_argument('--lr','--learning_rate',default=1e-5,type=int)
+parser.add_argument('--lr','--learning_rate',default=1e-3,type=int)
 args = parser.parse_args()
 
 root_path = "dataset"
@@ -28,7 +30,7 @@ model=MatrixFactorization(num_users=train_num_users*args.batch,
                           num_items=train_num_items*args.batch,
                           num_factors=100).to(device)
 optimizer = optim.Adam(model.parameters(),lr=args.lr)
-criterion = torch.nn.MSELoss().to(device)
+criterion = RMSELoss()
 
 if __name__=="__main__":
     train = Train(model=model,
